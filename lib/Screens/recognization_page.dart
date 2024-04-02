150006cf9fb2a716e5de68b963078384a52d0c7f
Rachidoulgour <olgor@hotmail.com>
2024-04-02 22:59:29 +0200

    recognization_page finished

--- lib/Screens/recognization_page.dart
+++ lib/Screens/recognization_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
 
 class RecognizePage extends StatefulWidget {
   final String path;
@@ -9,8 +10,41 @@ class RecognizePage extends StatefulWidget {
 }
 
 class _RecognizePageState extends State<RecognizePage> {
+  bool _isBusy = false;
+  TextEditingController controller = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    final InputImage inputImage = InputImage.fromFilePath(widget.path);
+    processImage(inputImage);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text("recognize page")),);
+    return Scaffold(
+        appBar: AppBar(title: const Text("recognize page")),
+        body: _isBusy
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : Container(
+              padding: const EdgeInsets.all(20),
+                child: TextFormField(
+                controller: controller,
+                decoration: const InputDecoration(hintText: "Text goes here")
+              )));
+  }
+
+  void processImage(InputImage image) async{
+    final textRecognizer = TextRecognizer(script: TextRecognitionScript.latin);
+    setState(() {
+      _isBusy = true;
+    });
+    final RecognizedText recognizedText = await textRecognizer.processImage(image);
+    controller.text = recognizedText.text;
+    setState(() {
+      _isBusy = false;
+    });
   }
 }
